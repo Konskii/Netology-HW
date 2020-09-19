5f19b97fa14e9953b0bc20138b2facc51c2230a0
Артём Скрипкин <artem@iMac-Artem.local>
2020-09-19 15:48:10 +0400

    BruteForce Operation class completed
    TO DO: Написать код раздающий индексы для экземпляров операций.

--- Operations/Operations/Code/BruteFroce Operation.swift
+++ Operations/Operations/Code/BruteFroce Operation.swift
@@ -8,18 +8,54 @@
 import Foundation
 class BruteForceOperation: Operation {
     ///Пароль, с котороым будет сравниваться результат брутфорса.
-    let inputPassword: String
-    var startIndexArray = [Int]()
-    
-    var endIndexArray = [Int]()
-    let maxIndexArray: Int
+    private var password: String
+    ///Найденный пароль
+    var foundPass: String?
+    var startIndex: [Int]
+    var endIndex: [Int]
+    let maxIndex = Int()
+    var currentIndex: [Int]
     
     ///Массив знаков
-    let characterArray: [String]
+    let characters = Consts.characterArray
     
-    required init(inputPassword: String, characterArray: [String]) {
-        self.inputPassword = inputPassword
-        self.maxIndexArray = characterArray.count
-        self.characterArray = characterArray
+    required init(currentPassword: String, startIndex: [Int], endIndex: [Int]) {
+        self.startIndex = startIndex
+        self.endIndex = endIndex
+        self.password = currentPassword
+        self.currentIndex = startIndex
+    }
+    
+    
+    override func main() {
+        if isCancelled {
+            return
+        }
+        
+        while true {
+            ///Генерируемый пароль
+            let pass = characters[startIndex[0]] + characters[startIndex[1]] + characters[startIndex[2]] + characters[startIndex[3]]
+            
+            ///Проверка на то, сходится ли пароль
+            if password == pass {
+                foundPass = pass
+            } else {
+                ///Если пароль не сходится, то проверяем не дошли ли мы до конца индексов
+                if currentIndex.elementsEqual(endIndex) {
+                    cancel()
+                    break
+                }
+                
+                ///Если все ок, то увеличваем индекс, с конца. Если индекс который мы хотим увеличить равен maxIndex, то увеличиваем слева стоящий от него, а его самого возвращаем к startIndex[3]
+                for index in (0 ..< currentIndex.count).reversed() {
+                    guard currentIndex[index] < maxIndex - 1 else {
+                        currentIndex[index] = 0
+                        continue
+                    }
+                    currentIndex[index] += 1
+                    break
+                }
+            }
+        }
     }
 }
