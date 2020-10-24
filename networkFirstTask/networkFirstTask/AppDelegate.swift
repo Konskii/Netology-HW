9f5e563d889ced4ca9d158e4dee07e78e97ea71b
Артём Скрипкин <artem@iMac-Artem.local>
2020-10-24 13:06:18 +0400

    init commit

---
+++ networkFirstTask/networkFirstTask/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  networkFirstTask
+//
+//  Created by Артём Скрипкин on 24.10.2020.
+//  Copyright © 2020 Артём Скрипкин. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        return true
+    }
+    
+}
+
