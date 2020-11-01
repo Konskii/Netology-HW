9c78244a9ada56c47023ee6db2b342aafdcc778a
Артём Скрипкин <artem@iMac-Artem.local>
2020-11-02 00:13:35 +0400

    init
    created third vc

---
+++ networkThirdTask/networkFirstTask/Controllers/ThirdViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ThirdViewController.swift
+//  networkFirstTask
+//
+//  Created by Артём Скрипкин on 02.11.2020.
+//  Copyright © 2020 Артём Скрипкин. All rights reserved.
+//
+
+import UIKit
+class ThirdViewController {
+    
+}
