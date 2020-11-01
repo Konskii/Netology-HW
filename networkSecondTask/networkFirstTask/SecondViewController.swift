aedbac92301abe2dc9b4fd65315e3bd484cd6146
Артём Скрипкин <artem@iMac-Artem.local>
2020-11-01 22:56:12 +0400

    init impl

---
+++ networkSecondTask/networkFirstTask/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  networkFirstTask
+//
+//  Created by Артём Скрипкин on 01.11.2020.
+//  Copyright © 2020 Артём Скрипкин. All rights reserved.
+//
+
+import UIKit
+class SecondViewController: UIViewController {
+    
+}
